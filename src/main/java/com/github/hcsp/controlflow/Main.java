6db0d4f6c585c4e927cfe64fff22997a2b8a2861
19855 <1985504469@qq.com>
2022-04-11 15:13:22 +0800

    .

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,13 +10,9 @@ public class Main {
      * @return 如果该年份是闰年，返回true，否则返回false
      */
     public static boolean isLeapYear(int year) {
-        if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            }else {
-                return false;
-            }
-        } else if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
         } else {
             return false;
